efed6307937c7301266e419a7e99191c205120a9
fary254 <127880269+fary254@users.noreply.github.com>
2024-02-14 16:19:00 +0000

    beta 0.1
    project start

---
+++ DebugINFO.py
@@ -0,0 +1,42 @@
+import psutil
+import time
+import os
+
+def get_process_info(pid):
+    p = psutil.Process(pid)
+    memory_info = p.memory_info()
+    gpu_memory = p.memory_info().vms
+    memory_addr = hex(id(p))
+    memfullinfo = p.memory_full_info()
+    connections = p.connections()
+    io_counters = p.io_counters()
+    threads = p.num_threads()
+    OPENFILES = p.open_files()
+    status = p.status()
+    return memory_info, gpu_memory, memory_addr, memfullinfo, connections, io_counters, threads, OPENFILES, status
+
+def monitor_process(pid):
+    try:
+        while True:
+            memory_info, gpu_memory, memory_addr, memfullinfo, connections, io_counters,threads, OPENFILES, status = get_process_info(pid)
+            print(f"Process memory: {memory_info.rss} bytes")
+            print(f"GPU memory: {gpu_memory} bytes")
+            print(f"Memory address: {memory_addr}")
+            print(f"status: {status}")
+            print("                                 ")
+            print(f"Memory full info: {memfullinfo}")
+            print("                                 ")
+            print(f"connections: {connections}")
+            print("                                 ")
+            print(f"io counters: {io_counters}")
+            print(f"opened files: {OPENFILES}")
+            print(f"threads: {threads}")
+
+            time.sleep(1)
+            os.system('clear')
+    except KeyboardInterrupt:
+        pass
+
+if __name__ == "__main__":
+    pid = int(input("Enter the PID of the process to monitor: "))
+    monitor_process(pid)
