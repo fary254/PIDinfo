b3ee2523142f1365b35534fc48f8d9b1cdf1c89a
fary254 <127880269+fary254@users.noreply.github.com>
2024-02-20 17:44:52 +0200

    beta 3
    functions and overall operation have been optimized

--- DebugINFO.py
+++ DebugINFO.py
@@ -1,25 +1,20 @@
 import psutil
 import time
 import os
-
-def get_process_info(pid):
-    p = psutil.Process(pid)
-    memory_info = p.memory_info()
-    gpu_memory = p.memory_info().vms
-    memory_addr = hex(id(p))
-    memfullinfo = p.memory_full_info()
-    connections = p.connections()
-    io_counters = p.io_counters()
-    threads = p.num_threads()
-    OPENFILES = p.open_files()
-    status = p.status()
-    terminal = p.terminal()
-    return memory_info, gpu_memory, memory_addr, memfullinfo, connections, io_counters, threads, OPENFILES, status, terminal
-
 def monitor_process(pid):
     try:
         while True:
-            memory_info, gpu_memory, memory_addr, memfullinfo, connections, io_counters,threads, OPENFILES, status, terminal = get_process_info(pid)
+            p = psutil.Process(pid)
+            memory_info = p.memory_info()
+            gpu_memory = p.memory_info().vms
+            memory_addr = hex(id(p))
+            memfullinfo = p.memory_full_info()
+            connections = p.connections()
+            io_counters = p.io_counters()
+            threads = p.num_threads()
+            OPENFILES = p.open_files()
+            status = p.status()
+            terminal = p.terminal()
             print(f"Process memory: {memory_info.rss} bytes")
             print(f"GPU memory: {gpu_memory} bytes")
             print(f"Memory address: {memory_addr}")
